97704549240573c57679b05b6d77a30c6029e517
mf0x <mikefox9000@gmail.com>
2023-10-16 15:22:19 -0400

    original answer tune plays with start button

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const msgField = document.getElementById('message-field')
+const startBtn = document.getElementById('start-btn')
 const playBtn = document.getElementById('play-btn')
 const pauseBtn = document.getElementById('pause-btn')
+const stopBtn = document.getElementById('stop-btn')
 
 // Array that holds both the random tune and the user's current selected tune
 const boolArray = new Array(80).fill(true)
@@ -17,7 +19,7 @@ const box = document.querySelectorAll(".box")
 
 box.forEach((el) => { 
     el.addEventListener('click', handleClick)
-    el.innerHTML = boolArray[el.dataset.boxNumber]
+    // el.innerHTML = boolArray[el.dataset.boxNumber]
     // console.log(el.dataset.boxNumber)
 })
 
@@ -62,9 +64,9 @@ function handleClick(e) {
         boolArray[checkedBox] = !boolArray[checkedBox]
     }
 
-    box.forEach((el) => { 
-        el.innerHTML = boolArray[el.dataset.boxNumber]
-    }) 
+    // box.forEach((el) => { 
+    //     el.innerHTML = boolArray[el.dataset.boxNumber]
+    // }) 
     
     if (winTest(boolArray)) {
         msgField.innerText = "YOU WON"
@@ -97,6 +99,7 @@ function playNote(level) {
 let index = 0;
 function playLoop() {
     let pause = false
+    let stop = false
     pauseBtn.onclick = () => {
         pause = !pause
         if (pause === false) playLoop()
@@ -107,7 +110,7 @@ function playLoop() {
             box[j + index].style.borderColor = 'lime'
             setTimeout(function() {
                 box[j + index].style.borderColor = 'magenta'
-            }, 62.5)                
+            }, 25)                
             if (box[j + index].dataset.checked === "1") {
                 playNote(5 - j)
             }
@@ -116,6 +119,27 @@ function playLoop() {
         if (index > 79) index = 0
         if (pause === false) playLoop()
     }, 125)
+
+    stopBtn.onclick = () => { console.log('stop button dont work yet!') }
 }
 
 playBtn.onclick = () => { playLoop() }
+
+
+
+startBtn.onclick = () => { listen() }
+
+const answerArray = [...boolArray]
+
+function listen() {
+    for (i = 0; i < 80; i += 5) {
+        // setTimeout(function() {
+            for (let j = 0; j < 5; j++) {
+                if (answerArray[i + j] === false) {
+                    console.log(i * 25)
+                    setTimeout(function() { playNote(5 - j) }, i * 25)
+                }
+            }
+        // }, i + 125)
+    }
+}
