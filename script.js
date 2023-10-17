75103863156de305ccc2ca088c79572c9c637521
mf0x <mikefox9000@gmail.com>
2023-10-17 12:52:18 -0400

    add next button, resets game state and restarts game

--- script.js
+++ script.js
@@ -5,12 +5,30 @@ const startBtn = document.getElementById('start-btn')
 const playBtn = document.getElementById('play-btn')
 const pauseBtn = document.getElementById('pause-btn')
 const stopBtn = document.getElementById('stop-btn')
+const nextBtn = document.getElementById('next-btn')
+
+// Musical parameters
+const stepChoices = [1/12, 1/6, 1/4, 1/3, 5/12]
+let step
+
+const basePitchChoices = [55]
+for (let i = 0; i < 60; i++) { basePitchChoices.push(basePitchChoices[i] * (2 ** stepChoices[0])) }
+let basePitch
+
+const waveformChoices = ['sine', 'triangle', 'square', 'sawtooth']
+let waveform
+
+let volume = 1
 
 // Array that holds both the random tune and the user's current selected tune
 const boolArray = new Array(80).fill(true)
+let answerArray = [...boolArray]
 
-// Populate boolArray with random tune at every 4th column
+// Populate boolArray with random tune
 function randomTune(difficulty) {
+    boolArray.fill(true)
+    console.log(boolArray)
+
     let density
     if (difficulty === 0) density = 20
     else if (difficulty === 1 || difficulty === 2) density = 10
@@ -20,82 +38,77 @@ function randomTune(difficulty) {
         let ii = i + Math.floor(Math.random() * 5)
         boolArray[ii] = false
 
-        if (difficulty === 2) {
-            if (Math.floor(Math.random() * 8) === 0) {
+        if (difficulty === 2 && Math.floor(Math.random() * 8) === 0) {
                 let iii = i + 5 + Math.floor(Math.random() * 5)
                 boolArray[iii] = false
-            }
         }
     }
+
+    answerArray = [...boolArray]
+
+    step = 2 ** stepChoices[Math.floor(Math.random() * stepChoices.length)]
+    basePitch = basePitchChoices[Math.floor(Math.random() * basePitchChoices.length)]
+    waveform = waveformChoices[Math.floor(Math.random() * waveformChoices.length)]
+    if (basePitch < 220 && waveform === 'sine') volume = 3
+
+    console.log(basePitch)
+    console.log(step)
+    console.log(waveform)
 }
 
-randomTune(2)
+randomTune(0)
 
-// Musical fine parameters
-const basePitchChoices = [55]
-for (let i = 0; i < 60; i++) { basePitchChoices.push(basePitchChoices[i] * (2 ** (1/12))) }
-let basePitch = basePitchChoices[Math.floor(Math.random() * basePitchChoices.length)]
-
-const stepChoices = [1/12, 1/6, 1/4, 1/3, 5/12]
-let step = stepChoices[Math.floor(Math.random() * stepChoices.length)]
-
-let multiplier = 2 ** step
-const waveformChoices = ['sine', 'triangle', 'square', 'sawtooth']
-let waveform = waveformChoices[Math.floor(Math.random() * waveformChoices.length)]
-
-let volume = 1
-if (basePitch < 220 && waveform === 'sine') volume = 3
-
-console.log(basePitch)
-console.log(step)
-console.log(waveform)
-
-// Preserve the random tune as boolArray is changed by the user
-const answerArray = [...boolArray]
 
 // Get the grid
 const box = document.querySelectorAll(".box")
 
-box.forEach((el) => { el.addEventListener('click', handleClick) })
+function resetBoard() {
+    box.forEach((el) => { el.addEventListener('click', handleClick) })
 
-function handleClick(e) {
-    const thisBox = parseInt(e.target.dataset.boxNumber)
-    const colFirstBox = thisBox - (thisBox % 5)
-    let checkedBox = -1
+    function handleClick(e) {
+        const thisBox = parseInt(e.target.dataset.boxNumber)
+        const colFirstBox = thisBox - (thisBox % 5)
+        let checkedBox = -1
 
-    // Check if any box in the clicked column is already checked; if so, get its box-number
-    for (let i = colFirstBox; i < colFirstBox + 5; i++) {
-        i = i.toString()
-        const currentBox = document.querySelector(`[data-box-number="${i}"]`)
-        if (currentBox.dataset.checked === '1') {
-            checkedBox = parseInt(currentBox.dataset.boxNumber)
-            break
+        // Check if any box in the clicked column is already checked; if so, get its box-number
+        for (let i = colFirstBox; i < colFirstBox + 5; i++) {
+            i = i.toString()
+            const currentBox = document.querySelector(`[data-box-number="${i}"]`)
+            if (currentBox.dataset.checked === '1') {
+                checkedBox = parseInt(currentBox.dataset.boxNumber)
+                break
+            }
+        }
+
+        if (checkedBox === -1) {
+            e.target.style.backgroundColor = 'blue'
+            e.target.dataset.checked = '1'
+            boolArray[thisBox] = !boolArray[thisBox]
+        } else if (checkedBox === thisBox) {
+            e.target.style.backgroundColor = 'black'
+            e.target.dataset.checked = '0'
+            boolArray[thisBox] = !boolArray[thisBox]
+        } else {
+            e.target.style.backgroundColor = 'blue'
+            e.target.dataset.checked = '1'
+            boolArray[thisBox] = !boolArray[thisBox]
+            const currentBox = document.querySelector(`[data-box-number="${checkedBox}"]`)
+            currentBox.style.backgroundColor = 'black'
+            currentBox.dataset.checked = '0'
+            boolArray[checkedBox] = !boolArray[checkedBox]
+        }
+
+        console.log(boolArray)
+
+
+        if (winTest(boolArray)) {
+            msgField.innerText = "YOU WON"
+            box.forEach((el) => { el.removeEventListener('click', handleClick) })         
         }
     }
-
-    if (checkedBox === -1) {
-        e.target.style.backgroundColor = 'blue'
-        e.target.dataset.checked = '1'
-        boolArray[thisBox] = !boolArray[thisBox]
-    } else if (checkedBox === thisBox) {
-        e.target.style.backgroundColor = 'black'
-        e.target.dataset.checked = '0'
-        boolArray[thisBox] = !boolArray[thisBox]
-    } else {
-        e.target.style.backgroundColor = 'blue'
-        e.target.dataset.checked = '1'
-        boolArray[thisBox] = !boolArray[thisBox]
-        const currentBox = document.querySelector(`[data-box-number="${checkedBox}"]`)
-        currentBox.style.backgroundColor = 'black'
-        currentBox.dataset.checked = '0'
-        boolArray[checkedBox] = !boolArray[checkedBox]
-    }
-
-    if (winTest(boolArray)) {
-        msgField.innerText = "YOU WON"
-        box.forEach((el) => { el.removeEventListener('click', handleClick) })         
-    }
 }
+
+resetBoard()
 
 function winTest(arr) {
     const numUniqueVals = new Set(arr);
@@ -106,7 +119,7 @@ function winTest(arr) {
 const context = new AudioContext()
 
 function playNote(level) {
-    const pitch = basePitch * (multiplier ** (level - 1))
+    const pitch = basePitch * (step ** (level - 1))
     console.log(pitch)
     const o = context.createOscillator()
     const g = context.createGain()
@@ -173,45 +186,17 @@ stopBtn.onclick = () => {
 
 playBtn.addEventListener("click", playLoop)
 
-/*
+nextBtn.addEventListener("click", newLevel)
 
-if stopped
-    start - plays answer
-    play - plays board
-    pause - nothing
-    stop - nothing
+function newLevel() {
+    box.forEach((el) => {
+        el.style.borderColor = 'magenta'
+        el.style.backgroundColor = 'black' 
+        el.dataset.checked = '0'
+    })
+   
+    randomTune(0)
+    resetBoard()
 
-if answer-playing
-    start - stops answer & plays answer
-    play - nothing
-    pause - nothing
-    stop - stops answer
-
-if board playing
-    start - stops board & plays answer
-    play - stops board & plays board
-    pause - pauses board
-    stop - stops board
-
-if board paused
-    start - stops board & plays answer
-    play - stops board & plays board
-    pause - unpauses board
-    stop - stops board
-
-*/
-
-// const gameState = {
-//     stopped: true;
-//     answer: false;
-//     playing: false;
-//     paused: false;
-// }
-
-
-// let playing =  false
-
-// function playPause() {
-//     if playing === playLoop()
-
-// }
+    msgField.innerText = ''
+}
